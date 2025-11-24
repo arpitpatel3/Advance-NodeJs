cde90c1ee4e5671a1615911365f7182743aa10c7
arpitpatel3 <arpit.patel3@cognizant.com>
2025-11-24 19:35:27 +0530

    Update config.js

--- Day2/CaseStudy1/modules/config.js
+++ Day2/CaseStudy1/modules/config.js
@@ -1,1 +1,7 @@
+export const PORT = 4000;
+export const KB_BASE = 1000;
+
+export const MAX_FILE_SIZE = 5 * 1024 * 1024; 
+export const PRICE_PER_KB = 0.001;
+export const UPLOAD_DIR = new URL("../uploads/", import.meta.url).pathname;
 
