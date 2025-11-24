c25492b473d5867f9dd6670a261b7f8c5ebc3044
arpitpatel3 <arpit.patel3@cognizant.com>
2025-11-24 16:03:08 +0530

    Update cart.js

--- Day2/BookStore/bookstore/cart.js
+++ Day2/BookStore/bookstore/cart.js
@@ -1,1 +1,10 @@
+function getUserCart(userId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["JS101", "NODE201"]);
+    }, 200);
+  });
+}
+
+module.exports = { getUserCart };
 
